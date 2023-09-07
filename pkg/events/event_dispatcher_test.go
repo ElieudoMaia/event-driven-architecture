936db5f55fd88ea640a9ec317ddbf7cd4549b8c2
Elieudo Maia <elieudo.maia@g4flex.com.br>
2023-09-07 20:51:37 -0300

    feat: add remove method to eventDispatcher

--- pkg/events/event_dispatcher_test.go
+++ pkg/events/event_dispatcher_test.go
@@ -128,6 +128,29 @@ func (suite *EventDispatcherTestSuite) TesteEventDispatcherDispatch() {
 	eh.AssertNumberOfCalls(suite.T(), "Handle", 1)
 }
 
+func (suite *EventDispatcherTestSuite) TestEventDispatchRemove() {
+	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler2)
+	suite.Nil(err)
+	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event2.GetName(), &suite.handler3)
+	suite.Nil(err)
+
+	suite.eventDispatcher.Remove(suite.event.GetName(), &suite.handler)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+	assert.Equal(suite.T(), &suite.handler2, suite.eventDispatcher.handlers[suite.event.GetName()][0])
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event2.GetName()]))
+
+	suite.eventDispatcher.Remove(suite.event.GetName(), &suite.handler2)
+	suite.Equal(0, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+	assert.Equal(suite.T(), &suite.handler3, suite.eventDispatcher.handlers[suite.event2.GetName()][0])
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event2.GetName()]))
+}
+
 // Execute all tests of the suite
 func TestSuite(t *testing.T) {
 	suite.Run(t, new(EventDispatcherTestSuite))
